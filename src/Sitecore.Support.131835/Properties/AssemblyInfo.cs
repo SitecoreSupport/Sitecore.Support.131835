20c582592be600472cea755b9cfbe82a78e7f2c7
Chee How Yap <yho@sitecore.net>
2018-05-08 10:28:33 +0800

    Commit Patch

---
+++ src/Sitecore.Support.131835/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.131835")]
+[assembly: AssemblyProduct("Sitecore.Support.131835")]
+[assembly: ComVisible(false)]
